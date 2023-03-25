40307fdc505309a8f68f6811320c9d0646b19408
BlueFury6 <bluefury6@gmail.com>
2023-03-25 14:22:11 -0700

    Added descriptions, fixed headers, added recents.

---
+++ graphs.js
@@ -0,0 +1,32 @@
+const graphs = [
+    {
+        Name: "No Graph Selected",
+        Description: "Try selecting a graph from the 'Graphs' tab above! All graphs are made using Desmos.",
+        API_Link: "https://www.desmos.com/calculator/i9w1u6rxg8",
+        Image_Link: ""
+    },
+    {
+        Name: "Graphability",
+        Description: "Notability is an app that can be used on iPads as a writing based note-taking tool. Using Desmos, I recreated this app as a graph.",
+        API_Link: "https://www.desmos.com/calculator/fo96wobara",
+        Image_Link: ""
+    },
+    {
+        Name: "Desmos Pong",
+        Description: "This is my recreation of the famous videogame 'Pong' within Desmos. The recomended(and default) difficulty is the maximum, as the lower difficulties are a bit buggy. Your score is in seconds survived when playing against the max AI setting.",
+        API_Link: "https://www.desmos.com/calculator/a6k3idycje",
+        Image_Link: "DesmosPongGraph.png"
+    },
+    {
+        Name: "Color Wheel",
+        Description: "Color Wheel Description",
+        API_Link: "https://www.desmos.com/calculator/tot7i3q2cm",
+        Image_Link: "ColorWheelGraph.png"
+    },
+    {
+        Name: "Star Simulator V.1",
+        Description: "Color Wheel Description",
+        API_Link: "https://www.desmos.com/calculator/xgunumf7ti",
+        Image_Link: "StarSimulatorV.1Graph.png"
+    }
+]
