8ed9c764cdee2d01ed9edf818ec843d8e5937832
BlueFury6 <bluefury6@gmail.com>
2023-03-25 20:15:55 -0700

    added a featured graph section

--- graphs.js
+++ graphs.js
@@ -3,30 +3,35 @@ const graphs = [
         Name: "No Graph Selected",
         Description: "Try selecting a graph from the 'Graphs' tab above! All graphs are made using Desmos.",
         API_Link: "https://www.desmos.com/calculator/i9w1u6rxg8",
-        Image_Link: ""
+        Image_Link: "",
+        Feature: false
     },
     {
         Name: "Graphability",
         Description: "Notability is an app that can be used on iPads as a writing based note-taking tool. Using Desmos, I recreated this app as a graph.",
         API_Link: "https://www.desmos.com/calculator/fo96wobara",
-        Image_Link: ""
+        Image_Link: "GraphabilityGraph.png",
+        Feature: true
     },
     {
         Name: "Desmos Pong",
-        Description: "This is my recreation of the famous videogame 'Pong' within Desmos. The recomended(and default) difficulty is the maximum, as the lower difficulties are a bit buggy. Your score is in seconds survived when playing against the max AI setting.",
+        Description: "This is my recreation of the famous videogame 'Pong' within Desmos. It allows for play on three different difficulties, and the ability to change the color scheme(just for fun). The recomended(and default) difficulty is the maximum, as the lower difficulties are a bit buggy. Your score is in seconds survived when playing against the max AI setting. This graph was the first one I made using the 'experimental' features of Desmos.",
         API_Link: "https://www.desmos.com/calculator/a6k3idycje",
-        Image_Link: "DesmosPongGraph.png"
+        Image_Link: "DesmosPongGraph.png",
+        Feature: true
     },
     {
         Name: "Color Wheel",
         Description: "Color Wheel Description",
         API_Link: "https://www.desmos.com/calculator/tot7i3q2cm",
-        Image_Link: "ColorWheelGraph.png"
+        Image_Link: "ColorWheelGraph.png",
+        Feature: false
     },
     {
         Name: "Star Simulator V.1",
         Description: "Color Wheel Description",
         API_Link: "https://www.desmos.com/calculator/os1mqchwd5",
-        Image_Link: "StarSimulatorV.1Graph.png"
+        Image_Link: "StarSimulatorV.1Graph.png",
+        Feature: true
     }
 ]
